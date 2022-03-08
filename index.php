056c7ad964ae43f9538f46c17db71cfaddcbc042
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-03-08 23:50:10 +0100

    exemple api read

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "localhost";
+$username = "Amine";
+$password = "teste123";
+$conn =new PDO("mysql:host=$servername;dbname=pizzas", $username, $password);
+
+
+$query = "SELECT * FROM pizza ";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+
+// echo "<pre>";
+// print_r($result);
+// echo "</pre>";
+
+if($stmt){
+
+
+    $array = array();
+    $array['data'] = array();
+
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        
+        $pizzas = array(
+            'id'=>$id,
+            'createdBy'=>$createdBy,
+            'date'=>$date,
+            'elements'=>$elements
+        );
+
+        array_push($array['data'],$pizzas);
+    }
+x
+    echo json_encode($array);
+    
+
+}else
+{
+    echo json_encode(
+        array('message'=>'No Pizza here')
+    );
+}
+
---
+++ scrpit.js
@@ -0,0 +1,66 @@
+
+
+// fetch("https://www.breakingbadapi.com/api/characters/1").then(promise=>promise.json()).then(data=>console.log(data)) 
+
+
+// async function get(){
+//     const response = await fetch("https://restcountries.com/v3.1/name/peru");
+//     const data = await response.json();
+//     console.log(data[0].name.official);
+//     document.querySelector('.content').innerHTML = data[0].name.official;
+// }
+// get();
+
+// new Vue({
+//     el:"#content",
+//     data: {
+//         countries: []
+//     },
+//     methods:{
+
+//         getAllData(){
+//             fetch("https://www.breakingbadapi.com/api/").then(result=>result.json()).then(data=>this.countries=data)
+//         }
+//     }
+
+// })
+
+
+let url  = "https://restcountries.com/v3.1/name/peru";
+
+// function fetchData(callback){
+//     fetch(url)
+//     .then(function(response){
+//         return response.json()
+//     }).then(function(data){
+//         callback(null,data)
+//     }).catch(function(err){
+//         callback(err,null)
+//     })
+// }
+// fetchData(function(err,data){
+//     if(err){
+//         console.error(err)
+//     }
+//     console.log(data)
+// })
+
+
+
+async function fetchData(){
+    let response = await fetch(url);
+    let json = await response.json();
+    return json;
+}
+
+async function main(){
+    try {
+        let json = await fetchData();
+        console.log(json)
+    } catch (error) {
+        console.error(error)
+    }
+}
+main();
+
+
